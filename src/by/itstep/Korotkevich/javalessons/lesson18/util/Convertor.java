30469ebe8b34f96b17e40cb35a7d681884991034
KorotkevichM <huawei.2k11@gmail.com>
2022-06-16 20:09:29 +0300

    add Converter class

---
+++ src/by/itstep/Korotkevich/javalessons/lesson18/util/Convertor.java
@@ -0,0 +1,14 @@
+package by.itstep.Korotkevich.javalessons.lesson18.util;
+
+public class Convertor {
+    public static String toString(int[] array) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]).append(" ");
+        }
+
+        return builder + "";
+    }
+}
