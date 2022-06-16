7101019f2bc1a1da28d47fe09d24ddb1157cfd53
KorotkevichM <huawei.2k11@gmail.com>
2022-06-16 21:47:25 +0300

    add ArraySearcher

--- src/by/itstep/Korotkevich/javalessons/lesson18/model/logic/ArraySearcher.java
+++ src/by/itstep/Korotkevich/javalessons/lesson18/model/logic/ArraySearcher.java
@@ -1,5 +1,33 @@
 package by.itstep.Korotkevich.javalessons.lesson18.model.logic;
 
 public class ArraySearcher {
-    
+    public static boolean check(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == value){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static int findFirstIndex(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == value){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int findLastIndex(int[] array, int value) {
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            if(array[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
